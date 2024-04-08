8a7a048fb49d2361953f7df468dd5897d079b0a5
Pedro Maximo <pedromaximocc@gmail.com>
2024-04-08 11:31:52 -0300

    Implement game features and UI updates
    Added several features to the game logic in app.js including handling player health, monster health, attack, special attack and healing functions. CSS styles settings were updated for disabled buttons and visibility statuses. The UI has been improved in the index.html to reflect the game state changes and display corresponding information to the user.

--- Section4 - Project/app.js
+++ Section4 - Project/app.js
@@ -1,13 +1,67 @@
+function randomNumberAttack(a1, a2) {
+    return Math.floor(Math.random() * (a1-a2));
+}
+
 const app = Vue.createApp({
     data(){
         return{
             monsterHealth:100,
             playerHealth: 100,
             gameLogs: [],
+            specialTimeout: false,
+            healTimeout: false,
+            winner: null,
+
+        }
+    },
+    computed:{
+        monsterHealthBar(){
+            return {width: this.monsterHealth + '%'}
+        },
+        playerHealthBar(){
+            return {width: this.playerHealth + '%'}
+        },
+        showWinner(){
+            return {hidden: this.winner === null, visible: this.winner !== null}
+        }
+    },
+    watch:{
+        playerHealth(value){
+            var gameOver = ''
+
+            if(value <=0 && this.monsterHealth <=0){
+                gameOver = {action: 'Draw !!'}
+                this.winner ='DRAW'
+            }else if(this.playerHealth <= 0){
+                gameOver = {action: 'Player Defeated. Monster Wins'}
+                this.winner ='MONSTER'
+            }
+            if(gameOver !== ''){
+                this.gameLogs.unshift(gameOver)
+            }
+
+        },
+        monsterHealth(value){
+            var gameOver = ''
+            if(value <=0 && this.playerHealth <=0){
+                gameOver = {action: 'Draw !!'}
+                this.winner ='DRAW'
+            }else if(this.monsterHealth <= 0){
+                gameOver = {action: 'Monster Defeated. Player Wins!'}
+                this.winner ='PLAYER'
+            }
+
+            if(gameOver !== ''){
+                this.gameLogs.unshift(gameOver)
+            }
         }
     },
 
+
     methods:{
+        restartGame(){
+            location.reload()
+        },
 
         logHandle(player, monster, action) {
             const actions = {
@@ -16,7 +70,8 @@ const app = Vue.createApp({
                     monsterLog: {action: 'Monster attacked Player', value: monster}
                 },
                 'heal': {
-                    playerLog: {action: 'Player heal', value: player},
+                    playerLog: {action: 'Player healed', value: player},
+                    monsterLog: {action: 'Monster healed', value: monster},
                     
                 },
                 'specialAttack': {
@@ -40,17 +95,35 @@ const app = Vue.createApp({
         },
 
         attack(){
-            var playerAttack =  randomNumberAttack()
-            var monsterAttack = randomNumberAttack()
+            var playerAttack =  randomNumberAttack(7, 2) +2
+            var monsterAttack = randomNumberAttack(9, 3) + 3
             this.monsterHealth -= playerAttack
             this.playerHealth -= monsterAttack
-            this.logHandle(playerAttack, monsterAttack)
+            this.logHandle(playerAttack, monsterAttack, 'attack')
 
         },
         specialAttack(){
 
+            var playerAttack = randomNumberAttack(13, 5) +3
+            var monsterAttack = randomNumberAttack(5, 0) + 3
+            this.monsterHealth -= playerAttack
+            this.playerHealth -= monsterAttack
+            this.logHandle(playerAttack, monsterAttack, 'specialAttack')
+            this.specialTimeout = true
+            setTimeout(() =>{
+                this.specialTimeout = false;
+            }, 3500)
+
         },
         heal(){
+            var playerHeal = randomNumberAttack(15, 5)
+            var monsterHeal = randomNumberAttack(4,0)
+            this.playerHealth += playerHeal
+            this.logHandle(playerHeal, monsterHeal, 'heal')
+            this.healTimeout = true
+            setTimeout(() =>{
+                this.healTimeout = false;
+            }, 3000)
 
         },
         surrender(){
@@ -61,6 +134,3 @@ const app = Vue.createApp({
 app.mount('#game')
 
 
-function randomNumberAttack() {
-    return Math.ceil(Math.random() * 30);
-}
