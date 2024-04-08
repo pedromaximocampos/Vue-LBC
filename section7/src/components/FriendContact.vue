33eb294de40b2e090f80ab387e8b1fe5cc466cf3
Pedro Maximo <pedromaximocc@gmail.com>
2024-04-08 17:26:01 -0300

    Add project setup information and new component
    Added a README file containing project setup information for section 7. New component named 'FriendContact' has been created under src/components. vue.config.js file is also added for Vue CLI service configuration. Created package-lock.json with updated dependencies.

---
+++ section7/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? 'Hide': 'Show'}}Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+</template>
+
+<script >
+export default {
+  data(){
+    return{
+      detailsAreVisible: false,
+      friend: {id:'21', name:'Pedro', phone:'31203913', email:'pedro@gmail.com'}
+    }
+  },
+  methods:{
+    toggleDetails(){
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
+
+
+<style >
+
+</style>
